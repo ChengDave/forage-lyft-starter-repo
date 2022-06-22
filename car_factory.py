fb05527ceae721d99ca71a82ad3d8ba70165a939
David Cheng <davidcheng@Davids-MacBook-Air.local>
2022-06-21 22:04:01 -0700

    Completed addition of carrigan and octoprime tires to factory method

--- car_factory.py
+++ car_factory.py
@@ -3,6 +3,8 @@ from engine.willoughby_engine import WilloughbyEngine
 from engine.sternman_engine import SternmanEngine
 from battery.spindler_battery import SpindlerBattery
 from battery.nubbin_battery import NubbinBattery
+from tire.carrigan_tire import CarriganTire
+from tire.octoprime_tire import OctoprimeTire
 from car import Car
 
 class CarFactory():
@@ -11,17 +13,27 @@ class CarFactory():
     Factory does not maintain any of the instances it creates
     """
 
-    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
-        return Car(CapuletEngine(current_mileage, last_service_mileage), SpindlerBattery(last_service_date, current_date))
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage, tire_wear_sensors):
+        return Car(CapuletEngine(current_mileage, last_service_mileage), 
+                   SpindlerBattery(last_service_date, current_date),
+                   OctoprimeTire(tire_wear_sensors))
 
-    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
-        return Car(WilloughbyEngine(current_mileage, last_service_mileage), SpindlerBattery(last_service_date, current_date))
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage, tire_wear_sensors):
+        return Car(WilloughbyEngine(current_mileage, last_service_mileage), 
+                   SpindlerBattery(last_service_date, current_date),
+                   OctoprimeTire(tire_wear_sensors))
 
-    def create_palindrome(current_date, last_service_date, warning_light_on):
-        return Car(SternmanEngine(warning_light_on), SpindlerBattery(last_service_date, current_date))
+    def create_palindrome(current_date, last_service_date, warning_light_on, tire_wear_sensors):
+        return Car(SternmanEngine(warning_light_on), 
+                   SpindlerBattery(last_service_date, current_date),
+                   OctoprimeTire(tire_wear_sensors))
 
     def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
-        return Car(WilloughbyEngine(current_mileage, last_service_mileage), NubbinBattery(last_service_date, current_date))
+        return Car(WilloughbyEngine(current_mileage, last_service_mileage), 
+                   NubbinBattery(last_service_date, current_date),
+                   CarriganTire(tire_wear_sensors))
 
     def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
-        return Car(CapuletEngine(current_mileage, last_service_mileage), NubbinBattery(last_service_date, current_date))
+        return Car(CapuletEngine(current_mileage, last_service_mileage), 
+                   NubbinBattery(last_service_date, current_date),
+                   CarriganTire(tire_wear_sensors))
--- serviceable.py
+++ serviceable.py
@@ -4,11 +4,3 @@ class Serviceable(ABC):
     @abstractmethod
     def needs_service(self) -> bool: 
         pass
-class Car(Serviceable):
-    def __init__(self, engine, battery):
-        self.__engine = engine
-        self.__battery = battery
-
-    def needs_service(self):
-        return self.__engine.needs_service() or self.__battery.needs_service()
-
